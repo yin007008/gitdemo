e381f16812630f9533c48735ab5bda61db7353c9
yin007008 <yin007008@163.com>
2015-07-01 19:04:32 +0800

    123

--- GitDemo.java
+++ GitDemo.java
@@ -4,6 +4,7 @@ public class GitDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcom to use github");
+		system.out.println("welcom to use github");
 	}	
 
 }
